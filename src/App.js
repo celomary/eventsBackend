5721be513ff94666711a95adc40db9141cc0a764
Mohamed El omary <elomarymohamed72@gmail.com>
2023-11-09 22:21:17 +0100

    excel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const sponsorHandleClick = () => {
+        const anchor = document.createElement('a');
+     anchor.setAttribute('href', "https://back-no4k.onrender.com/sponsors/excel");
+     anchor.setAttribute('download', "");
+     anchor.setAttribute("target", '_blank');
+     anchor.setAttribute('rel', "noreferrer");
+     anchor.click();
+     anchor.remove();
+  }
+
+  const subscribersHandleClick = () => {
+        const anchor = document.createElement('a');
+     anchor.setAttribute('href', "https://back-no4k.onrender.com/subscribers/excel");
+     anchor.setAttribute('download', "");
+     anchor.setAttribute("target", '_blank');
+     anchor.setAttribute('rel', "noreferrer");
+     anchor.click();
+     anchor.remove();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <section className="hero">
+        <h1>Bienvenue sur Mldfa Excel</h1>
+        <p>Téléchargez les données des sponsors et des participants au format Excel :</p>
+        <div className="btn-container">
+            <button onClick={sponsorHandleClick} className="btn">sponsors</button>
+            <button onClick={subscribersHandleClick} className="btn">participants</button>
+        </div>
+    </section>
   );
 }
 
